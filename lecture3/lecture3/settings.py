e297fdbdceef928eee156054d86a2aa0bfd6a095
codebr3ak <barakaralph@gmail.com>
2022-07-15 11:26:31 +0300

    playing around with views and urls

--- lecture3/lecture3/settings.py
+++ lecture3/lecture3/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
+    'hello', #install the app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
